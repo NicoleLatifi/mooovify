8a93c5ecaecf5d2512063b3a2189a2ff5092dfb3
NicoleLatifi <Nicole.Latifi@gmail.com>
2021-01-17 13:06:26 -0700

    Add props and state to getMovies from input

--- src/Search/Search.tsx
+++ src/Search/Search.tsx
@@ -1,11 +1,35 @@
 import './Search.css';
+import { Component } from "react";
 
-function Search(): JSX.Element {
-  return (
-    <div>
-      <h2>Search</h2>
-    </div>
-  );
+interface IProps {
+  getMovies: any
 }
+
+interface IState {
+  searchTerm: string
+}
+class Search extends Component<IProps, IState> {
+  constructor(props: IProps){
+    super(props)
+    this.state = {
+      searchTerm: ""
+    }
+  }
+
+  updateSearchTerm = (event: any) => {
+    // this.setState({ searchTerm: event.target.value })
+    // this.props.getMovies(this.state.searchTerm)
+    this.props.getMovies(event.target.value)
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Search</h2>
+        <input placeholder="movie title" onChange={this.updateSearchTerm}></input>
+      </div>
+    );
+  };
+};
 
 export default Search;
