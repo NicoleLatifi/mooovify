7ae0a4eeae4fb67734e6d99325721dcb9d2351cf
NicoleLatifi <Nicole.Latifi@gmail.com>
2021-01-17 13:03:01 -0700

    Receive props, render title and year

--- src/Movie/Movie.tsx
+++ src/Movie/Movie.tsx
@@ -1,13 +1,25 @@
 import './Movie.css';
 import Button from '../Button/Button'
+import { Component } from 'react';
 
-function Movie(): JSX.Element {
-  return (
-    <div>
-      <h3>Movie</h3>
-      <Button />
-    </div>
-  );
+interface IProps {
+  title: string,
+  year: number,
+}
+
+class Movie extends Component<IProps> {
+  constructor(props: IProps){
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>{this.props.title} ({this.props.year})</h3>
+        <Button />
+      </div>
+    );
+  }
 }
 
 export default Movie;
