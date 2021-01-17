1dba8bfa890fd85d8306cb120fe4569d7cfbbf87
NicoleLatifi <Nicole.Latifi@gmail.com>
2021-01-17 14:35:42 -0700

    Add props and state for Nominate and Remove

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,11 +1,30 @@
 import './Button.css';
+import { Component } from 'react';
 
-function Button(): JSX.Element {
-  return (
-    <button>
-      Button
-    </button>
-  );
+interface IProps {
+  isNominated: boolean,
+  toggleNomination: any,
+}
+
+interface IState {
+  buttonText: string
+}
+
+class Button extends Component<IProps, IState> {
+  constructor(props: IProps){
+    super(props)
+    this.state = {
+      buttonText: this.props.isNominated? "Remove" : "Nominate"
+    }
+  }
+
+  render() {
+    return (
+      <button onClick={this.props.toggleNomination}>
+        {this.state.buttonText}
+      </button>
+    );
+  }
 }
 
 export default Button;
