d2c789594a42ffecaa88ec84e05d468ca21b2ee4
NicoleLatifi <Nicole.Latifi@gmail.com>
2021-01-17 00:56:56 -0700

    Receive props and render movies

--- src/Results/Results.tsx
+++ src/Results/Results.tsx
@@ -1,15 +1,37 @@
 import './Results.css';
 import Movie from '../Movie/Movie'
+import { Component } from "react";
 
-function Results(): JSX.Element {
-  const movies = [<Movie />, <Movie />]
+interface IProps {
+  searchResults: []
+}
 
-  return (
+interface IState {
+  moviesToDisplay: JSX.Element[];
+}
+class Results extends Component<IProps, IState> {
+  constructor(props: IProps){
+    super(props)
+    this.state = {
+      moviesToDisplay: []
+    }
+  }
+  
+  componentDidMount() {
+    const foundMovies = this.props.searchResults.map(movie => {
+      return <Movie />
+    })
+    this.setState({ moviesToDisplay: foundMovies })
+  }
+  
+  render(){
+    return (
     <div>
       <h2>Results</h2>
-      {movies}
+      {this.state.moviesToDisplay}
     </div>
-  );
-}
+    );
+  };
+};
 
 export default Results;
