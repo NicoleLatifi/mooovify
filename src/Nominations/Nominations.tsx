b4bd94f13cf5c78a70b1802c768d8df282ff0808
NicoleLatifi <Nicole.Latifi@gmail.com>
2021-01-18 21:06:08 -0700

    Render nominated movies

--- src/Nominations/Nominations.tsx
+++ src/Nominations/Nominations.tsx
@@ -1,15 +1,41 @@
 import './Nominations.css';
 import Movie from '../Movie/Movie'
+import { Component } from "react";
 
-function Nominations(): JSX.Element {
-  // const movies = [<Movie />, <Movie />]
-  
-  return (
-    <div>
-      <h2>Nominations</h2>
-      {/* {movies} */}
-    </div>
-  );
+interface IProps {
+  nominationsToDisplay: {id: string, title: string, year: number}[],
+  updateNominations: any,
 }
+
+interface IState {
+  nominatedMovies: JSX.Element[];
+}
+
+class Nominations extends Component<IProps, IState> {
+  constructor(props: IProps){
+    super(props)
+    this.state = {
+      nominatedMovies: []
+    };
+  };
+
+  componentDidUpdate(prevProps: IProps) {
+    if(this.props.nominationsToDisplay !== prevProps.nominationsToDisplay) {
+      const movies = this.props.nominationsToDisplay.map(movie => {
+        return <Movie id={movie.id} title={movie.title} year={movie.year} updateNominations={this.props.updateNominations} />
+      });
+      this.setState({ nominatedMovies: movies })
+    }
+  };
+  
+  render() {
+    return (
+      <div>
+        <h2>Nominations</h2>
+        {this.state.nominatedMovies}
+      </div>
+    );
+  };
+};
 
 export default Nominations;
